39ebd4f6cf3a54548257cb2a5430bd9a7162f3ae
Osama Hamed <osahamed@gmail.com>
2016-08-10 14:46:07 +0300

    Update CVCalendarView.swift

--- CVCalendar/CVCalendarView.swift
+++ CVCalendar/CVCalendarView.swift
@@ -1,149 +1,339 @@
 //
-//  CVAuxiliaryView.swift
-//  CVCalendar Demo
+//  CVCalendarView.swift
+//  CVCalendar
 //
-//  Created by Eugene Mozharovsky on 22/03/15.
-//  Copyright (c) 2015 GameApp. All rights reserved.
+//  Created by E. Mozharovsky on 12/26/14.
+//  Copyright (c) 2014 GameApp. All rights reserved.
 //
 
 import UIKit
 
-public final class CVAuxiliaryView: UIView {
-    public var shape: CVShape!
-    public var strokeColor: UIColor! {
-        didSet {
-            setNeedsDisplay()
-        }
-    }
+public typealias WeekView = CVCalendarWeekView
+public typealias CalendarView = CVCalendarView
+public typealias MonthView = CVCalendarMonthView
+public typealias Manager = CVCalendarManager
+public typealias DayView = CVCalendarDayView
+public typealias ContentController = CVCalendarContentViewController
+public typealias Appearance = CVCalendarViewAppearance
+public typealias Coordinator = CVCalendarDayViewControlCoordinator
+public typealias Date = CVDate
+public typealias CalendarMode = CVCalendarViewPresentationMode
+public typealias Weekday = CVCalendarWeekday
+public typealias Animator = CVCalendarViewAnimator
+public typealias Delegate = CVCalendarViewDelegate
+public typealias AppearanceDelegate = CVCalendarViewAppearanceDelegate
+public typealias AnimatorDelegate = CVCalendarViewAnimatorDelegate
+public typealias ContentViewController = CVCalendarContentViewController
+public typealias MonthContentViewController = CVCalendarMonthContentViewController
+public typealias WeekContentViewController = CVCalendarWeekContentViewController
+public typealias MenuViewDelegate = CVCalendarMenuViewDelegate
+public typealias TouchController = CVCalendarTouchController
+public typealias SelectionType = CVSelectionType
+
+public final class CVCalendarView: UIView {
+    // MARK: - Public properties
+    public var manager: Manager!
+    public var appearance: Appearance!
+    public var touchController: TouchController!
+    public var coordinator: Coordinator!
+    public var animator: Animator!
+    public var contentController: ContentViewController!
+    public var calendarMode: CalendarMode!
     
-    public var fillColor: UIColor! {
-        didSet {
-            setNeedsDisplay()
-        }
-    }
+    public var (weekViewSize, dayViewSize): (CGSize?, CGSize?)
     
-    public let defaultFillColor = UIColor.colorFromCode(0xe74c3c)
+    private var validated = false
     
-    private var radius: CGFloat {
+    public var firstWeekday: Weekday {
         get {
-            return (min(frame.height, frame.width) - 10) / 2
+            if let delegate = delegate {
+                return delegate.firstWeekday()
+            } else {
+                return .Sunday
+            }
         }
     }
     
-    public unowned let dayView: DayView
-    
-    public init(dayView: DayView, rect: CGRect, shape: CVShape) {
-        self.dayView = dayView
-        self.shape = shape
-        super.init(frame: rect)
-        strokeColor = UIColor.clearColor()
-        fillColor = UIColor.colorFromCode(0xe74c3c)
-        
-        layer.cornerRadius = 1 //5
-        backgroundColor = .clearColor()
+    public var shouldShowWeekdaysOut: Bool! {
+        if let delegate = delegate, let shouldShow = delegate.shouldShowWeekdaysOut?() {
+            return shouldShow
+        } else {
+            return false
+        }
     }
     
-    public override func didMoveToSuperview() {
-        setNeedsDisplay()
+    public var presentedDate: Date! {
+        didSet {
+            if let _ = oldValue {
+                delegate?.presentedDateUpdated?(presentedDate)
+            }
+        }
     }
     
-    public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    public override func drawRect(rect: CGRect) {
-        var path: UIBezierPath!
-        
-        if let shape = shape {
-            switch shape {
-            case .RightFlag: path = rightFlagPath()
-            case .LeftFlag: path = leftFlagPath()
-            case .Circle: path = circlePath()
-            case .Rect: path = rectPath()
-            case .Custom(let customPathBlock): path = customPathBlock(rect)
+    public var shouldAnimateResizing: Bool {
+        get {
+            if let delegate = delegate, should = delegate.shouldAnimateResizing?() {
+                return should
             }
             
-            switch shape {
-            case .Custom: break
-            default: path.lineWidth = 1
+            return true
+        }
+    }
+    
+    public var shouldAutoSelectDayOnMonthChange: Bool{
+        get {
+            if let delegate = delegate, should = delegate.shouldAutoSelectDayOnMonthChange?() {
+                return should
+            }
+            return true
+        }
+    }
+    
+    public var shouldAutoSelectDayOnWeekChange: Bool{
+        get {
+            if let delegate = delegate, should = delegate.shouldAutoSelectDayOnWeekChange?() {
+                return should
+            }
+            return true
+        }
+    }
+    
+    public var shouldScrollOnOutDayViewSelection: Bool{
+        get {
+            if let delegate = delegate, should = delegate.shouldScrollOnOutDayViewSelection?() {
+                return should
+            }
+            return true
+        }
+    }
+    
+    // MARK: - Calendar View Delegate
+    
+    @IBOutlet public weak var calendarDelegate: AnyObject? {
+        set {
+            if let calendarDelegate = newValue as? Delegate {
+                delegate = calendarDelegate
             }
         }
         
-        strokeColor.setStroke()
-        fillColor.setFill()
-        
-        if let path = path {
-            path.stroke()
-            path.fill()
+        get {
+            return delegate
         }
     }
     
-    deinit {
-        //println("[CVCalendar Recovery]: AuxiliaryView is deinited.")
+    public weak var delegate: CVCalendarViewDelegate? {
+        didSet {
+            if manager == nil {
+                manager = Manager(calendarView: self)
+            }
+            
+            if appearance == nil {
+                appearance = Appearance()
+            }
+            
+            if touchController == nil {
+                touchController = TouchController(calendarView: self)
+            }
+            
+            if coordinator == nil {
+                coordinator = Coordinator(calendarView: self)
+            }
+            
+            if animator == nil {
+                animator = Animator(calendarView: self)
+            }
+            
+            if calendarMode == nil {
+                loadCalendarMode()
+            }
+        }
+    }
+    
+    // MARK: - Calendar Appearance Delegate
+    
+    @IBOutlet public weak var calendarAppearanceDelegate: AnyObject? {
+        set {
+            if let calendarAppearanceDelegate = newValue as? AppearanceDelegate {
+                if appearance == nil {
+                    appearance = Appearance()
+                }
+                
+                appearance.delegate = calendarAppearanceDelegate
+            }
+        }
+        
+        get {
+            return appearance
+        }
+    }
+    
+    // MARK: - Calendar Animator Delegate
+    
+    @IBOutlet public weak var animatorDelegate: AnyObject? {
+        set {
+            if let animatorDelegate = newValue as? AnimatorDelegate {
+                animator.delegate = animatorDelegate
+            }
+        }
+        
+        get {
+            return animator
+        }
+    }
+    
+    // MARK: - Initialization
+    
+    public init() {
+        super.init(frame: CGRectZero)
+        hidden = true
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        hidden = true
+    }
+
+    /// IB Initialization
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        hidden = true
     }
 }
 
-extension CVAuxiliaryView {
-    public func updateFrame(frame: CGRect) {
-        self.frame = frame
-        setNeedsDisplay()
+// MARK: - Frames update
+
+extension CVCalendarView {
+    public func commitCalendarViewUpdate() {
+        if let _ = delegate, let contentController = contentController {
+            let contentViewSize = contentController.bounds.size
+            let selfSize = bounds.size
+            let screenSize = UIScreen.mainScreen().bounds.size
+            
+            let allowed = selfSize.width <= screenSize.width && selfSize.height <= screenSize.height
+            
+            if !validated && allowed {
+                let width = selfSize.width
+                let height: CGFloat
+                let countOfWeeks = CGFloat(6)
+                
+                let vSpace = appearance.spaceBetweenWeekViews!
+                let hSpace = appearance.spaceBetweenDayViews!
+                
+                if let mode = calendarMode {
+                    switch mode {
+                    case .WeekView:
+                        height = selfSize.height
+                    case .MonthView :
+                        height = (selfSize.height / countOfWeeks) - (vSpace * countOfWeeks)
+                    }
+                    
+                    // If no height constraint found we set it manually.
+                    var found = false
+                    for constraint in constraints {
+                        if constraint.firstAttribute == .Height {
+                            found = true
+                        }
+                    }
+                    
+                    if !found {
+                        addConstraint(NSLayoutConstraint(item: self, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .Height, multiplier: 1, constant: frame.height))
+                    }
+                    
+                    weekViewSize = CGSizeMake(width, height)
+                    dayViewSize = CGSizeMake((width / 7.0) - hSpace, height)
+                    validated = true
+                    
+                    contentController.updateFrames(selfSize != contentViewSize ? bounds : CGRectZero)
+                }
+            }
+        }
     }
 }
 
-extension CVAuxiliaryView {
-    func circlePath() -> UIBezierPath {
-        let arcCenter = CGPointMake(frame.width / 2, frame.height / 2)
-        let startAngle = CGFloat(0)
-        let endAngle = CGFloat(M_PI * 2.0)
-        let clockwise = true
-        
-        let path = UIBezierPath(arcCenter: arcCenter, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
-        
-        return path
+// MARK: - Coordinator callback
+
+extension CVCalendarView {
+    public func didSelectDayView(dayView: CVCalendarDayView) {
+        if let controller = contentController {
+            //change by Osama hamed to check selected date not before current date.
+            if dayView.date.convertedDate()!.compare(NSDate()) == .OrderedAscending {
+                return
+            }
+            presentedDate = dayView.date
+            
+            delegate?.didSelectDayView?(dayView, animationDidFinish: false)
+            controller.performedDayViewSelection(dayView) // TODO: Update to range selection
+        }
+    }
+}
+
+// MARK: - Convenience API
+
+extension CVCalendarView {
+    public func changeDaysOutShowingState(shouldShow: Bool) {
+        contentController.updateDayViews(shouldShow)
     }
     
-    func rightFlagPath() -> UIBezierPath {
-//        let appearance = dayView.calendarView.appearance
-//        let offset = appearance.spaceBetweenDayViews!
-        
-        let flag = UIBezierPath()
-        flag.moveToPoint(CGPointMake(bounds.width / 2, bounds.height / 2 - radius))
-        flag.addLineToPoint(CGPointMake(bounds.width, bounds.height / 2 - radius))
-        flag.addLineToPoint(CGPointMake(bounds.width, bounds.height / 2 + radius ))
-        flag.addLineToPoint(CGPointMake(bounds.width / 2, bounds.height / 2 + radius))
-        
-        let path = CGPathCreateMutable()
-        CGPathAddPath(path, nil, circlePath().CGPath)
-        CGPathAddPath(path, nil, flag.CGPath)
-        
-        return UIBezierPath(CGPath: path)
+    public func toggleViewWithDate(date: NSDate) {
+        contentController.togglePresentedDate(date)
     }
     
-    func leftFlagPath() -> UIBezierPath {
-        let flag = UIBezierPath()
-        flag.moveToPoint(CGPointMake(bounds.width / 2, bounds.height / 2 + radius))
-        flag.addLineToPoint(CGPointMake(0, bounds.height / 2 + radius))
-        flag.addLineToPoint(CGPointMake(0, bounds.height / 2 - radius))
-        flag.addLineToPoint(CGPointMake(bounds.width / 2, bounds.height / 2 - radius))
-        
-        let path = CGPathCreateMutable()
-        CGPathAddPath(path, nil, circlePath().CGPath)
-        CGPathAddPath(path, nil, flag.CGPath)
-        
-        return UIBezierPath(CGPath: path)
+    public func toggleCurrentDayView() {
+        contentController.togglePresentedDate(NSDate())
     }
     
-    func rectPath() -> UIBezierPath {
-//        let midX = bounds.width / 2
-        let midY = bounds.height / 2
-        
-        let appearance = dayView.calendarView.appearance
-        let offset = appearance.spaceBetweenDayViews!
-        
-        print("offset = \(offset)")
-        
-        let path = UIBezierPath(rect: CGRectMake(0 - offset, midY - radius, bounds.width + offset / 2, radius * 2))
-        
-        return path
+    public func loadNextView() {
+        contentController.presentNextView(nil)
+    }
+    
+    public func loadPreviousView() {
+        contentController.presentPreviousView(nil)
+    }
+    
+    public func changeMode(mode: CalendarMode, completion: () -> () = {}) {
+        if let selectedDate = coordinator.selectedDayView?.date.convertedDate() where calendarMode != mode {
+            calendarMode = mode
+            
+            let newController: ContentController
+            switch mode {
+            case .WeekView:
+                contentController.updateHeight(dayViewSize!.height, animated: true)
+                newController = WeekContentViewController(calendarView: self, frame: bounds, presentedDate: selectedDate)
+            case .MonthView:
+                contentController.updateHeight(contentController.presentedMonthView.potentialSize.height, animated: true)
+                newController = MonthContentViewController(calendarView: self, frame: bounds, presentedDate: selectedDate)
+            }
+            
+            
+            newController.updateFrames(bounds)
+            newController.scrollView.alpha = 0
+            addSubview(newController.scrollView)
+            
+            UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+                self.contentController.scrollView.alpha = 0
+                newController.scrollView.alpha = 1
+            }) { _ in
+                self.contentController.scrollView.removeAllSubviews()
+                self.contentController.scrollView.removeFromSuperview()
+                self.contentController = newController
+                completion()
+            }
+        }
+    }
+}
+
+// MARK: - Mode load 
+
+private extension CVCalendarView {
+    func loadCalendarMode() {
+        if let delegate = delegate {
+            calendarMode = delegate.presentationMode()
+            switch delegate.presentationMode() {
+                case .MonthView: contentController = MonthContentViewController(calendarView: self, frame: bounds)
+                case .WeekView: contentController = WeekContentViewController(calendarView: self, frame: bounds)
+            }
+            
+            addSubview(contentController.scrollView)
+        }
     }
 }
